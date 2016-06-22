da754f2cd45a3626a73bc6682bc43766a4ad94ad
silas <silas@PC-Silas>
2016-06-22 20:27:46 -0300

    alteração do silas

--- arduino.php
+++ arduino.php
@@ -4,7 +4,7 @@
 echo "ID Logado = ".$_GET['idLogado']."<br>";
 echo "RFID Logado = ".$_GET['rfidLogado']."<br>";
 echo "ID Local = ".$_GET['idLocal']."<br>";
-
+ECHO "Teste do Silas";
 ?>
 Arduino
 
